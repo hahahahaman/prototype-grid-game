274fbf012ae5310fdc27a68712ecfb12980a4778
Ed Ye <hahahadude@gmail.com>
2015-10-19 02:45:05 -0400

    update build file so that path to shared object files can be relative

--- build.lisp
+++ build.lisp
@@ -1,14 +1,22 @@
 (require 'sb-posix)
 
-;; (load #P"~/quicklisp/setup.lisp")
-(push (merge-pathnames "lib/" *default-pathname-defaults*)
-      asdf:*central-registry*)
-(ql:quickload :matrix-transforms)
+(load #P"~/quicklisp/setup.lisp")
 
+(ql:quickload :alexandria
+              :cl-glfw3
+              :cl-opengl
+              :cl-soil
+              :fset
+              :glkit
+              :trivial-garbage
+              :defenum
+              :swank)
+
+(asdf:oos 'asdf:load-op 'matrix-transforms)
 (sb-ext:save-lisp-and-die "mt.bin"
                           :toplevel (lambda ()
                                       (sb-posix:putenv
-                                       (format nil "SBCL_HOME=~A" 
+                                       (format nil "SBCL_HOME=~A"
                                                #.(sb-ext:posix-getenv "SBCL_HOME")))
                                       (matrix-transforms:game)
                                       0)
